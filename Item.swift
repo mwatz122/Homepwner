3fdb1a1065b0dc7de1a19a993c89f2e56481596a
Matthew A. Watzman <mwatz122@gmail.com>
2017-06-11 23:30:36 -0400

    added Item class

---
+++ Item.swift
@@ -0,0 +1,25 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Matthew Watzman on 6/11/17.
+//  Copyright © 2017 Matthew Watzman. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        
+        super.init()
+    }
+}
