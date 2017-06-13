76c3d01de31ef3c567f8440aedc71e001b940087
Matthew A. Watzman <mwatz122@gmail.com>
2017-06-13 09:12:16 -0400

    Added ItemStore class

---
+++ ItemStore.swift
@@ -0,0 +1,22 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Matthew Watzman on 6/13/17.
+//  Copyright © 2017 Matthew Watzman. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item
+    {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+}
