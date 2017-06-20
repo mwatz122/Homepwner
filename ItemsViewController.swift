151cb541fe6db1b15f77a354be3d812628c26a7b
Matthew A. Watzman <mwatz122@gmail.com>
2017-06-20 07:25:53 -0400

    implemented tableView numberOfRowsInSection

--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -10,4 +10,8 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
 }
