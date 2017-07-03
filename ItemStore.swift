829577dbf9c8149e2d46a48c1730140fe3f36356
Matthew A. Watzman <mwatz122@gmail.com>
2017-07-03 09:28:39 -0400

    implemented removeItem function

--- ItemStore.swift
+++ ItemStore.swift
@@ -19,4 +19,10 @@ class ItemStore {
         
         return newItem
     }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
 }
