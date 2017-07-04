63ae1ed9c9dc1aae005e35034851aa7d4de36486
Matthew A. Watzman <mwatz122@gmail.com>
2017-07-03 22:55:02 -0400

    Added alert action sheet when user deletes an item in the list

--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -48,11 +48,28 @@ class ItemsViewController: UITableViewController {
             
             let item = itemStore.allItems[indexPath.row]
             
-            // Remove the item from the store
-            itemStore.removeItem(item)
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
             
-            // Also remove that row from the table view with an animation
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler:
+            {
+                (action) -> Void in
+                
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+                
+                // Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
         }
     }
     
