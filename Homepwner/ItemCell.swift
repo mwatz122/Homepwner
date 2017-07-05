436d7bdc6adb9f93563a1c39a88bc83aa526f2d9
Matthew A. Watzman <mwatz122@gmail.com>
2017-07-05 09:28:21 -0400

    added new ItemCell class

---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Matthew Watzman on 7/5/17.
+//  Copyright © 2017 Matthew Watzman. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
